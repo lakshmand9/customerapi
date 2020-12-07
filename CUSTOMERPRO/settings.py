ca044e077ba1bc4f25fd7699317d5f5bbe06a03f
vanaja <vanaja.kokkula@gmail.com>
2020-12-07 17:06:19 +0530

    first commit

--- CUSTOMERPRO/settings.py
+++ CUSTOMERPRO/settings.py
@@ -56,11 +56,14 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-       'http://localhost:3000',
+    'http://localhost:3000',
+] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://localhost:3000',
 ]
-
 
 ROOT_URLCONF = 'CUSTOMERPRO.urls'
 
